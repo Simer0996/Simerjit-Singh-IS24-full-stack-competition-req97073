066216133d1d017508a32027fe287de912e6bc31
Simer0996 <singhsimer09@gmail.com>
2023-03-30 10:55:12 -0700

    adding some functionality to edit form

--- client/src/components/forms/EditProductForm.jsx
+++ client/src/components/forms/EditProductForm.jsx
@@ -13,19 +13,19 @@ const EditProductForm = ({ setProductName,
             <form onSubmit={handleSubmit} className="w-[300px]">
                 <div className="form-group">
                     <span className="flex"><label htmlFor="productName" className="text-xl">Product Name</label><p className="text-red-600">*</p></span>
-                    <input type="text" className="w-full p-2" id="productName" placeholder="Enter name of the product" onChange={(e) => setProductName(e.target.value)} />
+                    <input type="text" className="w-full p-2" id="productName" placeholder="Enter name of the product" onChange={(e) => setProductName(e.target.value)} required />
                 </div>
                 <div className="form-group">
                     <span className="flex"><label htmlFor="productOwnerName" className="text-xl">Product Owner</label><p className="text-red-600">*</p></span>
-                    <input type="text" className="w-full p-2" id="productOwnerName" placeholder="Enter the product Owner" onChange={(e) => setProductOwnerName(e.target.value)} />
+                    <input type="text" className="w-full p-2" id="productOwnerName" placeholder="Enter the product Owner" onChange={(e) => setProductOwnerName(e.target.value)} required />
                 </div>
                 <div className="form-group">
                     <span className="flex"><label htmlFor="Developers" className="text-xl">Developers</label><p className="text-red-600">*</p></span>
-                    <input type="text" className="w-full p-2" id="Developers" placeholder="Enter name of the Developers" onChange={handleChange} />
+                    <input type="text" className="w-full p-2" id="Developers" placeholder="Enter name of the Developers" onChange={handleChange} required />
                 </div>
                 <div className="form-group">
                     <span className="flex"><label htmlFor="scrumMasterName" className="text-xl">Scrum master Name</label><p className="text-red-600">*</p></span>
-                    <input type="text" className="w-full p-2" id="scrumMasterName" placeholder="Enter the name of Scrum Master" onChange={(e) => setScrumMasterName(e.target.value)} />
+                    <input type="text" className="w-full p-2" id="scrumMasterName" placeholder="Enter the name of Scrum Master" onChange={(e) => setScrumMasterName(e.target.value)} required />
                 </div>
                 <div className="form-group">
                     <span className="flex"><label htmlFor="methodology" className="text-xl">Methodology</label><p className="text-red-600">*</p></span>
