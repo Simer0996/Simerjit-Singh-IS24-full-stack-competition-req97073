adddb5b1b97fbfb01cb847dd1ff4abf82882f2f6
Simer0996 <singhsimer09@gmail.com>
2023-03-31 00:21:58 -0700

    adding some mock data

--- server/db/mockData.js
+++ server/db/mockData.js
@@ -85,6 +85,577 @@ const data = [
         scrumMasterName: "Emma",
         startDate: "2022/08/12",
         methodology: "Agile",
+    }, {
+        id: "3",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "4",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "5",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "6",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "7",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "8",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "9",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "10",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "11",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "12",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "13",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "14",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "15",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "16",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "17",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "18",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "19",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "20",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "21",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "22",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "23",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "24",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "25",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "26",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "27",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "28",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "29",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "28",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "29",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "30",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "31",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "32",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "33",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "2",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "34",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "35",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "36",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "37",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "38",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma",
+        startDate: "2022/08/12",
+        methodology: "Agile",
+    }, {
+        id: "39",
+        productName: "FoodApp",
+        productOwnerName: "Leo",
+        developers: [
+
+            "Simer",
+            "Olivia",
+            "Bruna"
+        ],
+        scrumMasterName: "Emma9892",
+        startDate: "2022/08/08",
+        methodology: "Agile",
+    }, {
+        id: "40",
+        productName: "FoodApp40",
+        productOwnerName: "Leo",
+        developers: [
+            "Lee",
+            "Singh",
+
+        ],
+        scrumMasterName: "Emma1",
+        startDate: "2022/08/09",
+        methodology: "Agile",
     }
 
 ]
