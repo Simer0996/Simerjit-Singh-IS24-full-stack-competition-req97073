1a77a0e2609b0bd584365cbeb0b0f76bcc64bb17
Simer0996 <singhsimer09@gmail.com>
2023-03-30 19:22:30 -0700

    Adding about page of instructions, how to use apps

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-
+import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import PrimaryNav from "./components/navigation/PrimaryNav";
 
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
